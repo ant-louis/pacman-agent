e025a652085d16cbbf85eff59ed11bf137c09119
Antoine Louis <antoinelouis@MBP-de-Antoine.lan>
2018-10-13 17:46:46 +0200

    bfs same (change name variables) and dfs ok

--- dfs.py
+++ dfs.py
@@ -1,14 +1,12 @@
 from pacman_module.game import Agent
 from pacman_module.pacman import Directions
-from collections import deque
 
 
 class PacmanAgent(Agent):
 
-    # variable to keep track of the dot foods left
     nb_foods = None
 
-    #List to contain the path to the next food iteù
+    #List to contain the path to the next food
     nextactions = list()
     
     def __init__(self, args):
@@ -24,10 +22,13 @@ class PacmanAgent(Agent):
         """ 
         Produce a backtrace of the actions taken to find the food dot, using the 
         recorded meta dictionary
+
         Arguments:
         ----------
-        - `state`: the current game state. 
+        - `state`: the current game state.
+
         - `meta`: dictionnary containing the path information from one node to another
+
         Return:
         -------
         - A list of legal moves as defined in `game.Directions`
@@ -47,13 +48,14 @@ class PacmanAgent(Agent):
         """
         Given a pacman state, computes a path from that state to a state
         where pacman has eaten one food dot.
+
         Arguments:
         ----------
-        - `state`: the current game state. 
+        - `state`: the current game state.
         """
 
-        # a FIFO queue
-        queue = deque()
+        # a stack for the nodes to explore
+        stack = list()
 
         # an empty set to maintain visited nodes
         visited = set()
@@ -62,12 +64,12 @@ class PacmanAgent(Agent):
         # key -> (parent state, action to reach child)
         meta = dict()
 
-        queue.append(state) #Append root
+        stack.append(state) #Append root
         meta[state] = (None, None)
 
-        while queue: # While not empty
+        while stack: # While not empty
             #Pick one available state
-            current_node = queue.popleft()
+            current_node = stack.pop()
 
             # We found one food dot so we stop and compute a path.
             if current_node.getNumFood() < self.nb_foods:
@@ -80,23 +82,23 @@ class PacmanAgent(Agent):
                 #Successor was already visited
                 if (successor[0].getPacmanPosition(), successor[0].getNumFood()) in visited:
                     continue
-                #Successor wasn't visisted, we enqueue it
-                if successor[0] not in queue:
+                #Successor wasn't visisted, we push it onto the stack
+                if successor[0] not in stack:
                     meta[successor[0]] = (current_node, successor[1]) # create metadata for these nodes
-                    queue.append(successor[0])
+                    stack.append(successor[0])
             
-            # add the current node to the visited set
             visited.add((current_node.getPacmanPosition(), current_node.getNumFood()))
          
-
     def get_action(self, state):
 
         """
         Given a pacman game state, returns a legal move.
+
         Arguments:
         ----------
         - `state`: the current game state. See FAQ and class
                    `pacman.GameState`.
+
         Return:
         -------
         - A legal move as defined in `game.Directions`.
