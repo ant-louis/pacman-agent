b04d614620ddcb7d0ddda6028870625eab04140b
“Antoine <antoine.louis02@outlook.fr>
2018-11-01 16:12:11 +0100

    Update folder pacman_module

--- Minimax_agent/minimax.py
+++ Minimax_agent/minimax.py
@@ -1,5 +1,7 @@
 from pacman_module.game import Agent
 from pacman_module.pacman import Directions
+import math
+
 
 
 class PacmanAgent(Agent):
@@ -10,6 +12,56 @@ class PacmanAgent(Agent):
         - `args`: Namespace of arguments from command-line prompt.
         """
         self.args = args
+        self.nb_ghosts = 0
+
+    def minimax_decision(self, state, nb_ghosts):
+        """
+        """
+        action_values = list()
+
+        pac_successors = state.generatePacmanSuccessors()
+        for pac_successor in pac_successors:
+            value = self.min_value(pac_successor, nb_ghosts)
+            action_values.append(value)
+        
+        return max(action_values)
+    
+    def max_value(self, state, ghost_index):
+        """
+        """
+        # Check the terminal test
+        if state.isWin() or state.isLose():
+            return state.getScore()  # Returns the utility value
+        
+        # Initialize value
+        value = - math.inf
+
+        pac_successors = state.generatePacmanSuccessors()
+        for pac_successor in pac_successors:
+            value = max(value, self.min_value(pac_successor, ghost_index))
+        
+        return value
+
+    def min_value(self, state, ghost_index):
+        """
+        """
+        # Check the terminal test
+        if state.isWin() or state.isLose():
+            return state.getScore()  # Returns the utility value
+        
+        # Initialize value
+        value = math.inf
+
+        ghost_successors = state.generateGhostSuccessors(ghost_index)
+        for ghost_successor in ghost_successors:
+            if ghost_index > 1:
+                value = min(value, self.min_value(ghost_successor, ghost_index-1))
+            else:
+                value = min(value, self.max_value(ghost_successor, self.nb_ghosts))
+
+        return value
+
+
 
     def get_action(self, state):
         """
@@ -22,4 +74,10 @@ class PacmanAgent(Agent):
         -------
         - A legal move as defined in `game.Directions`.
         """
-        return Directions.STOP
+        # Get the number of ghosts in the layout
+        self.nb_ghosts = state.getNumAgents() - 1
+
+        # Compute next move
+        next_move = self.minimax_decision(state, self.nb_ghosts)
+
+        return next_move
