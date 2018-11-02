2fdf09eb7630f5f3d01ac6b4b10835d6709e7e99
“Antoine <antoine.louis02@outlook.fr>
2018-11-02 12:27:48 +0100

    Implement a custom evaluation function.
    Works quite well for small and medium with depth of 5 but not at
    all for the large grid.
    We have to find the good weights for each criterion.

--- Part_2 - Minimax_agent/minimax.py
+++ Part_2 - Minimax_agent/minimax.py
@@ -14,6 +14,79 @@ class PacmanAgent(Agent):
         self.args = args
         self.nb_ghosts = 0
         self.depth = 5
+    
+    def manhattan_distance(self,xy1, xy2):
+        """Returns the Manhattan distance between points.
+        Arguments:
+        ----------
+        - `xy1`: the first point.
+        - `xy2`: the second point.
+
+        Returns:
+        ----------
+        The Manhattan distance between points xy1 and xy2.
+        """
+        return abs(xy1[0] - xy2[0]) + abs(xy1[1] - xy2[1])
+
+    def score_evaluation(self, state):
+        """Returns the score of the state.
+        Arguments:
+        ----------
+        - `state`: the current game state.
+
+        Returns:
+        ----------
+        The score of the given state.
+        """
+        return state.getScore()
+
+    def custom_evaluation(self, state):
+        """Returns a custum utility value of the state.
+        Arguments:
+        ----------
+        - `state`: the current game state.
+
+        Returns:
+        ----------
+        The custom utility value at a given state.
+        """
+        score = 0
+        ghost_dist = 0
+        closest_food = math.inf
+        closest_ghost = math.inf
+        nb_foods_left = math.inf
+
+        food_list = state.getFood().asList()
+        pacman_pos = state.getPacmanPosition()
+        ghost_list = state.getGhostPositions()
+        current_score = state.getScore()
+
+        # If pacman wins the game
+        if state.isWin():
+            return math.inf
+        # If pacman loses the game
+        if state.isLose():
+            return - math.inf
+
+        # Get the distance from pacman to the closest food
+        food_dist = [self.manhattan_distance(pacman_pos, food_pos) for food_pos in food_list]
+        closest_food = min(food_dist)
+
+        # Get the distances from pacman to the closest ghost
+        ghost_dist = [self.manhattan_distance(pacman_pos, ghost_pos) for ghost_pos in ghost_list]
+        closest_ghost = min(ghost_dist)
+        
+        # Get the number of foods left
+        nb_foods_left = len(food_list)
+
+        # Compute score
+        score = 1 * current_score + \
+                -1 * (1./closest_ghost) + \
+                -1 * closest_food + \
+                -1  * nb_foods_left
+
+        return score
+
 
     def minimax_decision(self, state, depth, nb_ghosts):
         """Returns the best legal action according to the minimax algorithm.
@@ -50,11 +123,11 @@ class PacmanAgent(Agent):
 
         Returns:
         ----------
-        Returns the max value of the successor nodes.
+        The max utility value of the successor nodes.
         """
         # Check the terminal test
         if state.isWin() or state.isLose() or depth == 0:
-            return state.getScore()  # Returns the utility value
+            return self.custom_evaluation(state)  # Returns the utility value
         
         # Initialize value
         value = - math.inf
@@ -75,11 +148,11 @@ class PacmanAgent(Agent):
 
         Returns:
         ----------
-        Returns the min value of the successor nodes.
+        The min utility value of the successor nodes.
         """
         # Check the terminal test
         if state.isWin() or state.isLose() or depth == 0:
-            return state.getScore()  # Returns the utility value
+            return self.custom_evaluation(state)  # Returns the utility value
         
         # Initialize value
         value = math.inf
@@ -92,8 +165,6 @@ class PacmanAgent(Agent):
                 value = min(value, self.max_value(ghost_successor[0], depth-1, self.nb_ghosts))
 
         return value
-
-
 
     def get_action(self, state):
         """
