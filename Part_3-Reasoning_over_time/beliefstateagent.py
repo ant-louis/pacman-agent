b256698040b636e66c06774d4932ac4438487878
“Antoine <antoine.louis02@outlook.fr>
2018-12-22 03:00:12 +0100

    Remove graphs and csv

--- Part_3-Reasoning_over_time/beliefstateagent.py
+++ Part_3-Reasoning_over_time/beliefstateagent.py
@@ -59,6 +59,7 @@ class BeliefStateAgent(Agent):
         for i, evidence in enumerate(evidences):
             # Reshape beliefStates to a 1D vector
             f = beliefStates[i].reshape(-1,1,order='F')
+            print(f)
 
             # Compute observation matrix
             index = self._get_cell(evidence[0], evidence[1])
